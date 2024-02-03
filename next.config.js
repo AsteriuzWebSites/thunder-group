0c223c6c002bc81f4097c73df7c6d617b104f057
Asteriuz <gutocebola@gmail.com>
2024-02-02 22:25:21 -0300

    Updated Site

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/thunder-group",
+  assetPrefix: "/thunder-group/",
   output: "export", // <=== enables static exports
   reactStrictMode: true,
 };
