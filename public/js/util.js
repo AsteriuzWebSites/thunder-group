b6521ec5be105c70809482e926e26b07198707b4
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-06 20:34:46 -0300

    Added Style Scrollbar

--- public/js/util.js
+++ public/js/util.js
@@ -50,3 +50,25 @@ document.addEventListener('click', function (e) {
     trailerModal.close();
   }
 });
+
+
+/* -------------------------------------------------------------------------- */
+/*                                  ScrollBar                                 */
+/* -------------------------------------------------------------------------- */
+
+const body = document.querySelector("body")
+
+window.addEventListener(
+  "scroll",
+  () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+      body.classList.add("scroll-bottom")
+    } else if (window.scrollY === 0) {
+      body.classList.add("scroll-top")
+    } else {
+      body.classList.remove("scroll-top")
+      body.classList.remove("scroll-bottom")
+    }
+  },
+  { passive: true }
+)
