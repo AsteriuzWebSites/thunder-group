e12ae5ac4d11e9ae7fdaac916e200c1304bfba73
Asteriuz <gutocebola@gmail.com>
2024-03-03 21:56:15 -0300

    CHanged JS

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,10 +3,8 @@ import Link from "next/link";
 import Image from "next/image";
 import FooterCategory from "./FooterCategory";
 import {
-  IoLogoFacebook,
   IoLogoInstagram,
   IoLogoWhatsapp,
-  IoLocationOutline,
   IoCallOutline,
   IoTimeOutline,
   IoLogoYoutube,
